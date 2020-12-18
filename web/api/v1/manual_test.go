f5799e4a3431dea7af47670e09719228390c1dfc
Ben Ye <yb532204897@gmail.com>
2020-12-18 13:19:05 -0500

    aa
    Signed-off-by: Ben Ye <yb532204897@gmail.com>

--- web/api/v1/manual_test.go
+++ web/api/v1/manual_test.go
@@ -8,6 +8,25 @@ import (
 	"testing"
 )
 
+func TestLabelNames1(b *testing.T) {
+	suite := promql.NewTestWithDir(b, "/home/yeya24/bb")
+	defer suite.Close()
+	api := &API{
+		Queryable:             suite.Storage(),
+		QueryEngine:           suite.QueryEngine(),
+	}
+
+	//req, err := http.NewRequest(http.MethodGet, `http://example.com/api/v1/labels?match[]={__name__!=""}`, nil)
+	//req, err := http.NewRequest(http.MethodGet, `http://example.com/api/v1/labels`, nil)
+	req, err := http.NewRequest(http.MethodGet, `http://example.com/api/v1/labels?match[]={account!=""}&match[]={alertname!=""}`, nil)
+	require.NoError(b, err)
+
+	//for i := 0; i < 1; i++ {
+	res := api.labelNames(req)
+	fmt.Println(res.data)
+	//}
+}
+
 func BenchmarkLabelNames(b *testing.B) {
 	suite := promql.NewTestWithDir(b, "/home/yeya24/bb")
 	defer suite.Close()
@@ -22,8 +41,8 @@ func BenchmarkLabelNames(b *testing.B) {
 	require.NoError(b, err)
 
 	b.ResetTimer()
-	//for i := 0; i < 1; i++ {
+	for i := 0; i < b.N; i++ {
 		res := api.labelNames(req)
 		fmt.Println(res.data)
-	//}
+	}
 }
